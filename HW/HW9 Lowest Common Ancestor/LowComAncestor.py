de7793bc23f1c08e8e5f7bce5eb0c4cf583e64c2
jrodmon <54284855+jrodmon@users.noreply.github.com>
2024-03-11 19:16:04 -0700

    Update LowComAncestor.py

--- HW/HW9 Lowest Common Ancestor/LowComAncestor.py
+++ HW/HW9 Lowest Common Ancestor/LowComAncestor.py
@@ -1,1 +1,34 @@
-stuff
+class TreeNode(object):
+
+    def __init__(self, val=0, left=None, right=None):
+
+        self.val = val
+
+        self.left = left
+
+        self.right = right
+         
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if not root:
+            return None
+        if p.val < root.val and q.val < root.val:
+            return self.lowestCommonAncestor(root.left,p,q)
+        elif p.val > root.val and q.val > root.val:
+            return self.lowestCommonAncestor(root.right,p,q)
+        else:
+            return root
+    
+if __name__ == "__main__":
+    root = TreeNode(4)
+    root.left = TreeNode(3)
+    root.left.left = TreeNode(1)
+    root.right = TreeNode(8)
+    root.right.left = TreeNode(5)
+    root.right.right = TreeNode(9)
+    
+    p = root.left
+    q = root.left.left
+    solution = Solution()
+    result = solution.lowestCommonAncestor(root,p,q)
+    print (result.val)
